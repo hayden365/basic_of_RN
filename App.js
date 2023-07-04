51c41882c3cf40113d2350d9268c0a6a3031faf4
hayden365 <ysmh100@gmail.com>
2023-07-04 23:59:39 +0900

    flexbox

--- App.js
+++ App.js
@@ -1,30 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<View>
-				<Text style={styles.dummyText}>Another piece of text!</Text>
+		<View style={styles.appContainer}>
+			<View style={styles.inputContainer}>
+				<TextInput style={styles.textInput} placeholder="Your course goal!" />
+				<Button title="Add Goal" />
 			</View>
-			<Text style={styles.dummyText}>Hello World!</Text>
-			<Button title="Tap me!" />
-			<StatusBar style="auto" />
+			<View>
+				<Text></Text>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+	appContainer: {
+		padding: 50,
 	},
-	dummyText: {
-		margin: 16,
-		padding: 16,
-		borderWidth: 2,
-		borderColor: 'blue',
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	textInput: {
+		borderWidth: 1,
+		borderColor: '#cccccc',
+		width: '80%',
+		marginRight: 8,
+		padding: 8,
 	},
 });
